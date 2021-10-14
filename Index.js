ca0d9e9be27c576fc64faa914daef6cc8542adc0
Sternicus <90805907+Sternicus@users.noreply.github.com>
2021-10-14 13:57:33 -0400

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,17 @@
+const PORT = 8000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+
+const app = express()
+
+const url = ''
+
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        console.log(html)
+    })
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
